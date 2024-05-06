1e416744124464b6ba6b55de86c2bbc305683f08
Mohamed Ahmed <73137157+mohamedS2020@users.noreply.github.com>
2024-05-06 07:08:40 +0300

    Register page and functions

--- screens/register.jsx
+++ screens/register.jsx
@@ -1,12 +1,19 @@
 import { router } from "expo-router";
-import React, { useState } from "react";
+import React, { useState , useEffect } from "react";
+import { MaterialCommunityIcons, Octicons } from "@expo/vector-icons";
 import {
   View,
   TextInput,
   Button,
   Text,
   Pressable,
+  TouchableOpacity,
   StyleSheet,
+  ImageBackground,
+  Image,
+  Keyboard,
+  Animated,
+  Easing,
 } from "react-native";
 import { register } from "../firebase/auth";
 import COLORS from "../constants/colors";
@@ -27,70 +34,166 @@ const Register = () => {
       setError(error);
     }
   };
+  const [logoOpacity] = useState(new Animated.Value(1));
 
+  useEffect(() => {
+    // Event listener for keyboard show
+    const keyboardShowListener = Keyboard.addListener("keyboardDidShow", () => {
+      Animated.timing(logoOpacity, {
+        toValue: 0, // Fade out
+        duration: 300, // Animation speed
+        easing: Easing.out(Easing.poly(4)), // Easing function for a smooth start
+        useNativeDriver: true, // Use native driver for better performance
+      }).start();
+    });
+
+    // Event listener for keyboard hide
+    const keyboardHideListener = Keyboard.addListener("keyboardDidHide", () => {
+      Animated.timing(logoOpacity, {
+        toValue: 1, // Fade in
+        duration: 300, // Animation speed
+        easing: Easing.out(Easing.poly(4)), // Easing function for a smooth start
+        useNativeDriver: true, // Use native driver for better performance
+      }).start();
+    });
+
+    // Cleanup function
+    return () => {
+      keyboardShowListener.remove();
+      keyboardHideListener.remove();
+    };
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Make Your Account</Text>
-      <TextInput
-        placeholder="Name"
-        value={userName}
-        onChangeText={setUserName}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Email"
-        value={email}
+    <ImageBackground
+      source={require("../assets/images/Register.jpeg")} // Replace with your image path
+      style={styles.background}
+      blurRadius={4} // Optional: if you want the background image to be blurred
+    >
+      <Animated.View style={[styles.logoContainer, { opacity: logoOpacity }]}>
+        <Image
+          source={require("../assets/images/Logo.png")}
+          style={styles.logo}
+        />
+        <Text style={styles.elegantText}>Elevate Every Day</Text>
+      </Animated.View>
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <MaterialCommunityIcons
+            name="email-outline"
+            size={24}
+            color="#858080"
+            style={styles.icon}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            keyboardType="email-address"
+value={email}
         onChangeText={setEmail}
-        style={styles.input}
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <Octicons
+            name="person"
+            size={24}
+            color="#858080"
+            style={styles.icon}
+          />
+          <TextInput style={styles.input} 
+placeholder="Name"
+value={userName}
+        onChangeText={setUserName}
+ />
+        </View>
+        <View style={styles.inputContainer}>
+          <Octicons name="lock" size={24} color="#858080" style={styles.icon} />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry
+value={password}
         onChangeText={setPassword}
-        secureTextEntry
-        style={styles.input}
-      />
-      <Button title="Register" onPress={handlePress} color={COLORS.primary} />
-      <Pressable onPress={() => router.replace("/(authenticate)/login")}>
-        <Text style={styles.link}>Login</Text>
-      </Pressable>
-      <Pressable onPress={() => router.replace("/(authenticate)/forgot")}>
-        <Text style={styles.link}>Forgot Password</Text>
-      </Pressable>
-      {error ? <Text style={styles.error}>{error.code}</Text> : null}
-    </View>
+          />
+        </View>
+        <TouchableOpacity onPress={handlePress} style={styles.button}>
+          <Text style={styles.buttonText}>Register</Text>
+        </TouchableOpacity>
+{error ? <Text style={styles.error}>{error.code}</Text> : null}
+      </View>
+    </ImageBackground>
   );
-};
+}
 
 const styles = StyleSheet.create({
+  background: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    resizeMode: "cover",
+  },
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-end", // Changed from "center" to "flex-end"
     alignItems: "center",
-    marginHorizontal: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 170, // Increased padding to push the content down
   },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
+  inputContainer: {
+    flexDirection: "row", // Align icon and text input
+    alignItems: "center", // Center items vertically
+    width: "100%",
+    height: 50,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "#858080",
+    borderRadius: 25,
+    paddingHorizontal: 15,
+    backgroundColor: "rgba(133, 128, 128, 0.4)", // Semi-transparent background
+    color: "black",
   },
   input: {
-    borderWidth: 1,
-    borderColor: COLORS.gray,
-    borderRadius: 8,
-    paddingVertical: 12,
-    paddingHorizontal: 20,
-    marginBottom: 20,
+    flex: 1,
+    height: "100%",
+    paddingHorizontal: 10,
+    fontWeight: "bold",
+  },
+  icon: {
+    marginRight: 10,
+  },
+  button: {
     width: "100%",
-  },
-  link: {
+    height: 50,
+    backgroundColor: "#FCC873",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 10,
-    color: COLORS.primary,
   },
-  error: {
-    color: "red",
-    marginTop: 10,
+  buttonText: {
+    color: "black",
+    fontSize: 18,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 100, // Adjust as needed for your logo size
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+  },
+  logo: {
+    width: 175, // Set the width of your logo
+    height: 175, // Set the height of your logo
+    resizeMode: "contain", // Keeps the aspect ratio intact
+  },
+  elegantText: {
+    fontFamily:'Quicksand-Bold',
+    marginTop: 13, // Space between logo and text
+    color: "#D3D3D3", // Light color for contrast
+    fontSize: 22, // Adjust as needed
   },
 });
+
 
 export default Register;
