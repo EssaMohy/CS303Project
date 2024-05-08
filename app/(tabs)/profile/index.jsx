dd43e0ae31d0539c54b838431c6b7026d5312da1
Idkmariam <mariamebrahim720@gmail.com>
2024-05-08 16:39:57 +0200

    edit

--- app/(tabs)/profile/index.jsx
+++ app/(tabs)/profile/index.jsx
@@ -1,9 +1,6 @@
-import { StyleSheet, Text, View, Button,TouchableOpacity,ScrollView,ImageBackground,Image, TextInput } from "react-native";
-import React, {useState} from "react";
-import { signOut} from "../../../firebase/auth"; // Import the signOut function from your Firebase authentication module
+import { StyleSheet, Text, View, Button, TouchableOpacity, ScrollView, Image, TextInput } from "react-native";
+import React, { useState } from "react";
 import { logout } from "../../../firebase/auth";
-import user from "../../../assets/images/icon.png";
-
 
 const Profile = () => {
   const handleSignOut = async () => {
@@ -16,7 +13,6 @@ const Profile = () => {
   };
 
   const [name, setName] = useState('');
-
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -28,104 +24,82 @@ const Profile = () => {
   };
 
   return (
-    <ImageBackground
-      source={require("../../../assets/images/gg.jpg")}
-      style={styles.background}
-      blurRadius={4}
-    >
-
     <View style={styles.container}>
-      <Text style={{color: "black",fontSize: 24, }}> My Profile</Text>
-     
-      <Image source={user}
-      style={styles.image}
-      ></Image>
-      
-      <TextInput
-          style={styles.input}
-          placeholder="Name"
-          value={name}
-          onChangeText={setName}
-        />
-       
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          value={email}
-          onChangeText={setEmail}
-          keyboardType="email-address"
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        />
-      
-      <TouchableOpacity onPress={handleSaveProfile} style={styles.button}>
-          <Text style={styles.buttonText}>Update</Text>
-        </TouchableOpacity>
+      <Text style={{ color: "black", fontSize: 24 }}> My Profile</Text>
 
-    <TouchableOpacity onPress={handleSignOut} style={styles.button}>
-      <Text style={styles.buttonText}>Logout</Text>
-    </TouchableOpacity>
-  </View>
-  </ImageBackground>
- 
+      <Image source={require("../../../assets/images/icon.png")} style={styles.image} />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        value={name}
+        onChangeText={setName}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+
+      <TouchableOpacity onPress={handleSaveProfile} style={styles.button}>
+        <Text style={styles.buttonText}>Update</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={handleSignOut} style={styles.button}>
+        <Text style={styles.buttonText}>Logout</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
 export default Profile;
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-  },
   container: {
     flex: 1,
     justifyContent: 'space-between',
     alignItems: 'center',
-    paddingTop: 50,
-    paddingBottom:50, // Adjust this value to increase or decrease the space between the top of the screen and the image
+    paddingTop: 20,
+    paddingBottom: 80,
+    backgroundColor: '#f0f0f0', // Off-white background color
   },
   image: {
     width: 170,
     height: 170,
-    borderRadius:100,
+    borderRadius: 100,
   },
-  textcont:{
-   alignContent:"center",
-   marginBottom:200,
-
-  },
-  text:{
-    fontSize: 24,
-    fontFamily: "Quicksand",
-    marginBottom: 0,
-    color: "white",
-  },
-
   input: {
     width: '80%',
-    height: 40,
+    height: 50,
     backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    borderRadius: 20,
+    borderRadius: 14,
     paddingLeft: 15,
     marginBottom: 10,
+    borderWidth: 1,  // Add border width
+    borderColor: '#FCC873',
   },
   button: {
-    width: '60%',
-    height: 50,
+    width: '80%',
+    height: 60,
     backgroundColor: '#FCC873',
-    borderRadius: 25,
+    borderRadius: 14,
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom:50,
+    marginBottom: 20,
   },
   buttonText: {
     color: 'black',
     fontSize: 18,
-  
   },
 });
