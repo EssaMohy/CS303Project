1e05dbb1dc149f86777d84ca272823500dd90043
Essam Mohy <essammohy67@gmail.com>
2024-05-10 18:22:52 +0300

    Update brands.js

--- data/brands.js
+++ data/brands.js
@@ -1,15 +1,23 @@
 const brands = [
   {
-    title: "Trending",
+    brand: "Casio",
+    image:
+      "https://egywatch.com/cdn/shop/files/1_b6b84b38-a81d-4ae9-ba00-fc20708e9e76_200x.png?v=1618618814",
   },
   {
-    title: "Popular",
+    brand: "SWatch",
+    image:
+      "https://egywatch.com/cdn/shop/files/2_ea22fd13-90b0-460d-a2c7-b04edba95667_200x.jpg?v=1618618817",
   },
   {
-    title: "New",
+    brand: "Tissot",
+    image:
+      "https://egywatch.com/cdn/shop/files/3_bac5fa30-554d-41d6-81ae-89d97e5bcb4a_200x.png?v=1618618820",
   },
   {
-    title: "Best Collection",
+    brand: "Boss",
+    image:
+      "https://egywatch.com/cdn/shop/files/4_0a3d286e-9dda-4c28-96f3-834cfe8c349c_200x.png?v=1618618824",
   },
 ];
 
