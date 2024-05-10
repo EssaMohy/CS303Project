4946fbb630221697cac9676de3f4da525bc69d05
Essam Mohy <essammohy67@gmail.com>
2024-05-10 23:41:12 +0300

    new screens

---
+++ app/(tabs)/cart/CheckOut.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const CheckOut = () => {
+  return (
+    <View>
+      <Text>CheckOut</Text>
+    </View>
+  );
+};
+
+export default CheckOut;
+
+const styles = StyleSheet.create({});
--- app/(tabs)/profile/_layout.jsx
+++ app/(tabs)/profile/_layout.jsx
@@ -4,6 +4,7 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="ProfileEdit" options={{ headerShown: false }} />
     </Stack>
   );
 }
